e3cc5a0c3d8b2819c7241e333f03eb3610353604
Sebastian Lammel <sebastianlammel@googlemail.com>
2014-12-20 23:45:41 +0100

    initial commit

---
+++ config.py
@@ -0,0 +1,16 @@
+__author__ = 'M1g4'
+
+import os
+import random
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+key = os.urandom(24)
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = key
+
+DATABASE_PATH = os.path.join(basedir, DATABASE)
